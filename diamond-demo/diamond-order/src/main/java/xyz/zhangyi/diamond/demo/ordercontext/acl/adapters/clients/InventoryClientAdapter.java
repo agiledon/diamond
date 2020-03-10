6bc562b5b6ca4bd5e9522cf529a4306c4cebc23b
yi <brucezhang@tom.com>
2020-03-10 11:56:19 +0800

    implements InventoryClientAdapter

--- diamond-demo/diamond-order/src/main/java/xyz/zhangyi/diamond/demo/ordercontext/acl/adapters/clients/InventoryClientAdapter.java
+++ diamond-demo/diamond-order/src/main/java/xyz/zhangyi/diamond/demo/ordercontext/acl/adapters/clients/InventoryClientAdapter.java
@@ -1,6 +1,8 @@
 package xyz.zhangyi.diamond.demo.ordercontext.acl.adapters.clients;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import xyz.zhangyi.diamond.demo.ordercontext.acl.ports.clients.InventoryClient;
 import xyz.zhangyi.diamond.demo.ordercontext.acl.ports.pl.CheckingInventoryRequest;
 import xyz.zhangyi.diamond.demo.ordercontext.acl.ports.pl.InventoryReviewResponse;
@@ -8,13 +10,18 @@ import xyz.zhangyi.diamond.demo.ordercontext.acl.ports.pl.LockingInventoryReques
 
 @Component
 public class InventoryClientAdapter implements InventoryClient {
+    private static final String INVENTORIES_RESOURCE_URL = "http://inventory-service/inventories";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public InventoryReviewResponse check(CheckingInventoryRequest request) {
-        return null;
+        return restTemplate.postForObject(INVENTORIES_RESOURCE_URL, request, InventoryReviewResponse.class);
     }
 
     @Override
-    public void lock(LockingInventoryRequest from) {
-
+    public void lock(LockingInventoryRequest request) {
+        restTemplate.put(INVENTORIES_RESOURCE_URL, request);
     }
 }
